fe0f643a6225aff838b070a63f3a529f1bb42024
Manuelo69 <nomail@gmail.com>
2024-08-09 10:29:58 +0200

    Primera subida

---
+++ src/main/java/cic/ejerc007_Manuel/Ejerc007ManuelApplication.java
@@ -0,0 +1,13 @@
+package cic.ejerc007_Manuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejerc007ManuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejerc007ManuelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cic/ejerc007_Manuel/Ejerc007ManuelApplicationTests.java
@@ -0,0 +1,13 @@
+package cic.ejerc007_Manuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejerc007ManuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
